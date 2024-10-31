6879d538d3e12c500f03d4286d3dcd9274ce9a49
FVVSANTOS <flaviovss@yahoo.com>
2024-10-31 16:13:59 -0300

    Envio dos dados dor formulário

--- imagelite/src/components/input/InputText.tsx
+++ imagelite/src/components/input/InputText.tsx
@@ -4,12 +4,13 @@ interface InputTextProps{
     style?: string;
     onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
     placeHolder?: string;
+    id: string;
 }
 
 export const InputText: React.FC<InputTextProps> = ({style, ...rest}: InputTextProps) => {
     return(
         <input type="text" 
                {...rest}
-               className={`${style} border px-3 py-2 rounded-lg text-gray-900`}/>
+               className={`${style} border-gray-400 border px-3 py-2 rounded-lg text-gray-900`}/>
     )
 }
