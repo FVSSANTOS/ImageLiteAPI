6f184a55858701834489026144c47f2163c618c3
FVSSANTOS <flaviovss@yahoo.com>
2024-10-30 21:55:22 -0300

    Criando formulário

--- imagelite/src/app/galeria/page.tsx
+++ imagelite/src/app/galeria/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Template, ImageCard, Button } from '@/components';
+import { Template, ImageCard, Button, InputText } from '@/components';
 import { Image } from '@/resources/image/image.resource';
 import { useImageService } from '@/resources/image/image.service';
 import { useState } from 'react';
@@ -43,9 +43,7 @@ export default function GaleriaPage(){
             <section className='flex flex-col items-center justify-center my-5'>
 
                 <div className='flex space-x-4'>
-                    <input type="text" 
-                           onChange={event => setQuery(event.target.value)}
-                           className='border px-3 py-2 rounded-lg text-gray-900' />
+                    <InputText placeHolder='Type Name or Tags' onChange={event => setQuery(event.target.value)}/>
                     <select  onChange={event => setExtension(event.target.value)}
                              className='border px-4 py-2 rounded-lg text-gray-900'>
                         <option value="">All formats</option>
